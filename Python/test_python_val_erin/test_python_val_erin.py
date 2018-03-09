6be913b798afae01a29ba3d092b2330ef0486010
Valentin Haveaux <32429241+ValentinHaveauxUCL@users.noreply.github.com>
2018-03-09 16:08:01 +0100

    MAJ AUTO 2018-03-09 16:08

---
+++ Python/test_python_val_erin/test_python_val_erin.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python2.7.10
+
+while 1:
+    inp = input("Qui es-tu? ")
+    if inp == "erin":
+        print("Pas de majuscule? ^^")
+    elif inp == "Erin":
+        print("Tu dois un bisou a ton copain <3")
+        break
+    else:
+        print("Dsl je ne te connais pas :p ")
+        
